463e71f741fa65b35a2a0cb139accc6af4588dfb
Alan Narvaez <alan6192@gmail.com>
2026-02-26 13:58:10 -0500

    Add PostgreSQL

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.connect()
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error("DB error:", err));
 const express = require("express");
 
 const app = express();
